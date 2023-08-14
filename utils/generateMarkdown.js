fd21955ee03fef1bbb9f9466a998021cbd327776
Alex <github@alexbi.shop>
2023-08-14 17:15:10 +0900

    Implemented renderLicenseLink()

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-
-
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
@@ -22,7 +18,23 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GPL 3.0":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "BSD 3":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    case "None":
+      return "";
+    // Default to no license
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
